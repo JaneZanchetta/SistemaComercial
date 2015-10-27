d9d0aba24a37a142eec27601d6957fe4335c8489
univ <kk@k>
2015-10-27 19:53:12 -0200

    Primeiro Commit: Enum UF

---
+++ src/main/java/br/univel/cadastro/UF.java
@@ -0,0 +1,22 @@
+package br.univel.cadastro;
+
+public enum UF {
+	PR("Paraná"),
+	SC("Santa Catarina"),
+	RS("Rio Grande do Sul"),
+	SP("São Paulo"),
+	ES("Espírito Santo"),
+	RJ("Rio de Janeiro");
+	
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	private UF(String nome) {
+		this.nome = nome;
+}
+	}
+	
