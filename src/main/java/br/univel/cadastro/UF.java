f98c304aaf9b6e6193da1cf4c2689f92ce2bb6b7
univ <kk@k>
2015-10-27 20:17:55 -0200

    Enum Genero, class Usuario

--- src/main/java/br/univel/cadastro/UF.java
+++ src/main/java/br/univel/cadastro/UF.java
@@ -1,14 +1,9 @@
 package br.univel.cadastro;
 
 public enum UF {
-	PR("Paraná"),
-	SC("Santa Catarina"),
-	RS("Rio Grande do Sul"),
-	SP("São Paulo"),
-	ES("Espírito Santo"),
-	RJ("Rio de Janeiro");
-	
-	
+	PR("Paraná"), SC("Santa Catarina"), RS("Rio Grande do Sul"), SP("São Paulo"), ES(
+			"Espírito Santo"), RJ("Rio de Janeiro");
+
 	private String nome;
 
 	public String getNome() {
@@ -17,6 +12,6 @@ public enum UF {
 
 	private UF(String nome) {
 		this.nome = nome;
-}
 	}
+}
 	
