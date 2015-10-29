0bce6a9ad472dc3848e1570d73630ca4b2018162
Admin <Admin@192.168.25.52>
2015-10-29 01:02:54 -0200

    Cliente e Unidade

---
+++ src/main/java/br/univel/cadastro/Unidade.java
@@ -0,0 +1,22 @@
+package br.univel.cadastro;
+
+
+public class Unidade {
+
+	private int id;
+	private String descricao;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
