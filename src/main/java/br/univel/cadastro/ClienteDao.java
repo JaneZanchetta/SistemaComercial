27aec82b5d8a24fcd78c030d9965592ec5c80f4a
Admin <Admin@192.168.25.52>
2015-10-30 00:10:59 -0200

    ClienteDao

---
+++ src/main/java/br/univel/cadastro/ClienteDao.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+/**
+ * @Author Jane Z
+ * 30/10/2015 00:05:39
+ */
+
+public interface ClienteDao {
+
+	public void create (Cliente c);
+
+	public Cliente read (int id);
+
+	public void update (Cliente c);
+
+	public void delete (Cliente c);
+
+	public Cliente search (Cliente c);
+
+	public List<Cliente> liste();
+}
