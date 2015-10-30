dc5e6004ebe540266fdb31850d292aea0f881f2d
Admin <Admin@192.168.25.52>
2015-10-29 23:39:58 -0200

    Alteração unidade, criação categoria

--- src/main/java/br/univel/cadastro/Unidade.java
+++ src/main/java/br/univel/cadastro/Unidade.java
@@ -1,22 +1,6 @@
 package br.univel.cadastro;
 
 
-public class Unidade {
-
-	private int id;
-	private String descricao;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	
-	
+public enum Unidade {
+	KG,UNID,PCT,PEÇA,CAIXA;
 }
